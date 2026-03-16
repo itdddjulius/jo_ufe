683be33b210ab6e7c4e4e3259b12b737e2ea2640
It Ddd <itddd.julius@gmail.com>
2026-03-16 16:49:26 +0000

    Delete public directory

--- public/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-require_once __DIR__ . '/../vendor/autoload.php';
-echo "<h1 style='background:black;color:white;padding:20px'>JO Universal File Editor</h1>";
-echo "<p style='color:white;background:black;padding:10px'>Starter project loaded.</p>";
-?>
